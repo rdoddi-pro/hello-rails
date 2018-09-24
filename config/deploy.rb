a1a7e4ac7ed11a90eb41d463ee592a4d0356abaa
Ramesh Doddi <rdoddi.pro@gmail.com>
2018-09-23 17:53:53 -0700

    Aded puma config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,37 +7,15 @@ set :repo_url, "git@github.com:rdoddi-pro/hello-rails.git"
 # User used to deploy the app to remote server
 set :user, "deployer"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :use_sudo, true
+set :deploy_via, :copy
+set :keep_releases, 2
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-set :deploy_to, "/home/deployer/www/hello-rails"
+set :log_level, :debug
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :rvm1_ruby_version, 'ruby-2.3.1'
+set :rvm_type, :user
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# set :linked_files, %w{config/application.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,5 @@
 server '35.167.112.143', user: 'deployer', roles: %w{app web}
 
 set :stage, :staging
-set :branch, "develop"
+set :deploy_to, "/home/deployer/www/hello-rails"
+set :branch, "feature/puma-config"
